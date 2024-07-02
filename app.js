57115756179a8f6270dddbb1c6680072ac5faf42
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-07-02 00:38:37 +0000

    adding ejs pages

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 export default app;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("On");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
