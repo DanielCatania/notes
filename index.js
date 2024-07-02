1f136785cf0182d664ed8d07dafce0096eea29b4
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-07-02 18:01:31 +0000

    adding new note form

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/new-note", (req, res) => {
+  res.render("new-note");
+});
+
+app.post("/add-note", (req, res) => {
+  res.json(req.body);
+});
+
 app.listen(PORT, () => {
   console.log(`SERVER ON PORT ${PORT}`);
 });
